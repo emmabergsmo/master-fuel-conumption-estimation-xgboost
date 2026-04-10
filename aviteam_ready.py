a974cccd5225dcd5b4cb7ffdd3b3007397778dc9
emmabergsmo <ebergsmo@gmail.com>
2026-04-10 14:37:57 +0200

    added aviteam-ready files

---
+++ aviteam_ready.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+db_path = "opensky_updated.sqlite"
+
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS aviteam_ready;")
+
+cursor.execute("""
+    CREATE TABLE aviteam_ready_phase AS
+    SELECT 
+        hex,
+        departure AS port_o,
+        arrival AS port_d,
+        callsign AS flight,
+        t,
+        lon,
+        lat,
+        postime,
+        alt_baro,
+        alt_geom,
+        gs,
+        track,
+        baro_rate,
+        squawk,
+        flight_id,
+        phase_id,
+        phase_name,
+        FALSE AS ground,
+        FALSE AS completed,
+        FALSE AS completed_adv,
+        NULL as r
+    FROM adsb_fuel_v2;
+""")
+
+conn.commit()
+conn.close()
+
+print("Tabellen opprettet.")
